fb82aa7de86ef754f678d58580526416ec0d52bb
Ewan Mellor <commits@ewanmellor.org>
2015-12-06 22:38:50 -0800

    Add a Mac framework.

---
+++ FBKVOController-OSX/FBKVOControllerOSX.h
@@ -0,0 +1,19 @@
+//
+//  FBKVOController-OSX.h
+//  FBKVOController-OSX
+//
+//  Created by Ewan Mellor on 12/6/15.
+//  Copyright © 2015 Kimon Tsinteris. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FBKVOController-OSX.
+FOUNDATION_EXPORT double FBKVOController_OSXVersionNumber;
+
+//! Project version string for FBKVOController-OSX.
+FOUNDATION_EXPORT const unsigned char FBKVOController_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBKVOController_OSX/PublicHeader.h>
+
+
